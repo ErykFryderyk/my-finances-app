5069bf63c42f8a88d766a97072669dcce3be73a4
Eryk Fryderyk <grzeniu098@gmail.com>
2020-12-29 00:03:01 +0100

    add main class

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,34 @@
-console.log('Hello World!');
+const input = document.querySelector('.input__payment');
+
+class App {
+    constructor(inputElem, elem){
+        this.inputElem = inputElem;
+        this.elem = elem;
+    }
+    start(){
+        console.log(this.inputElem);
+        console.log(this.elem)
+        // this.elem.innerHTML = 2;
+        const span = document.querySelector('span');
+        span.innerHTML = 'cześć';
+        this.elem.appendChild(span);
+    }
+}
+
+
+
+
+
+
+const app = new App({
+    inputElem: document.querySelector('.input__payment'),
+    elem: document.querySelector('.expenses__value').textContent,
+});
+
+
+
+input.addEventListener('keypress', (e) => {
+    if(e.key === "Enter" && input.value !== ''){
+        app.start();
+    }
+})
