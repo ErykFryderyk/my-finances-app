3512c28e5592e4b21a569c6d233ef2f4bebe0476
Eryk Fryderyk <grzeniu098@gmail.com>
2020-12-30 21:09:08 +0100

    add class App and create start method responsible for run this app.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,75 @@
+import { Expenses } from './expenses.js';
+
 const input = document.querySelector('.input__payment');
+const headerElem = document.querySelector('.header');
+
 
 class App {
-    constructor(inputElem, elem){
-        this.inputElem = inputElem;
-        this.elem = elem;
+    constructor({
+        headerElement, 
+        input,
+        expenses,
+        navBar,
+        payment
+    }){
+        this.headerElement = headerElement;
+        this.input = input;
+        this.expenses = expenses;
+        this.navBar = navBar;
+        this.payment = payment;
+
+        this.expensesObj = new Expenses();
     }
+
+    createButton(parentElement = this.headerElement){
+        const button = document.createElement('button');
+        button.innerText = 'CLICK!';
+
+        parentElement.append(button);
+    }
+
+    insertValue(e, paymentValue){
+        this.payment.innerHTML = paymentValue + ' zł';
+    }
+
+    createInput(){
+        const input = document.createElement('input');
+        input.classList.add('input__payment');
+        input.setAttribute('type', 'number');
+        input.setAttribute('value', 560);
+
+        input.addEventListener('keypress', (e) =>{
+            const paymentValue = input.value;
+            if(e.key === 'Enter' && input.value !== ''){
+                this.insertValue(e, paymentValue);
+                input.value = '';
+            }
+        })
+
+        this.navBar.appendChild(input);
+    }
+
+
+    //This method run app 
     start(){
-        console.log(this.inputElem);
-        console.log(this.elem)
-        // this.elem.innerHTML = 2;
-        const span = document.querySelector('span');
-        span.innerHTML = 'cześć';
-        this.elem.appendChild(span);
+        this.createInput();
+        
     }
 }
 
-
-
-
-
-
 const app = new App({
-    inputElem: document.querySelector('.input__payment'),
-    elem: document.querySelector('.expenses__value').textContent,
+    headerElement: document.querySelector('.header'),
+    input: document.querySelector('.input__payment'),
+    expenses: document.querySelector('.expenses__value'),
+    navBar: document.querySelector('.nav-bar'),
+    payment: document.querySelector('.payment__value'),
 });
 
+app.start();
 
 
-input.addEventListener('keypress', (e) => {
-    if(e.key === "Enter" && input.value !== ''){
-        app.start();
-    }
-})
+// input.addEventListener('keypress', (e) => {
+//     if(e.key === "Enter" && input.value !== ''){
+//         app.start();
+//     }
+// })
