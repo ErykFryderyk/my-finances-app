dabd44d588269524b726d2aa721c78e3ff5da17a
Eryk Fryderyk <grzeniu098@gmail.com>
2021-01-06 20:51:51 +0100

    update methods

--- assets/js/expenses.js
+++ assets/js/expenses.js
@@ -3,71 +3,79 @@ import { arrow, categoryIcon } from './svgfile.js';
 export  class Expenses{
     
     expensesWrapper = document.querySelector('.expenses-bar');
-    expensesBox = null;
+    expensesHeader = null;
 
     constructor(){
 
     }
 
-
-    createExpensesBar(){
-        const el = [] 
-        for (let i = 0; i < 2; i++) {
-            const div = document.createElement('div');
-            el[i].push(div);
-            expensesBox.addEventListener('click', function(){
-                console.log('SIEMA');
-            })
-        }
-
-        el[0].className = 
-        el[0]= document.createElement('div');
-        
-
-        expensesBox.className = 'expenses-bar__inner';
-        this.expensesBox = expensesBox;
-
-        // return expensesBar;
-        console.log(this.expensesBox);
-        this.createBoxIcon();
-        this.createBarIcon();
-        this.createExpensesBarTitle();
-        this.price();
-        this.expensesWrapper.appendChild(this.expensesBox);
-        console.log(this.expensesWrapper);
+    createElement(element, type, nameClass ){
+        element = document.createElement(`'${type}'`);
+        element.className = nameClass;
     }
 
-    createBarIcon(){
-        const barIcon = document.createElement('div');
-        barIcon.className = 'bar__icon';
-        barIcon.innerHTML = categoryIcon[0];
 
-        this.expensesBox.appendChild(barIcon);
-    }
-
-    createExpensesBarTitle(){
-        const title = document.createElement('h2');
-        title.className = 'bar-title'
-        title.innerHTML = "Nowa Kategoria";
-        this.expensesBox.appendChild(title);
-    }
-
-    createBoxIcon(){
+    createExpensesBarIcon(parentEl){
         const boxIcon = document.createElement('div');
 
-        boxIcon.className = 'box__icon';
+        boxIcon.className = 'expenses-bar__icon';
         boxIcon.innerHTML = arrow;
             
-        this.expensesBox.appendChild(boxIcon); 
+        parentEl.appendChild(boxIcon); 
     }
 
-    price(){
+    createBarIcon(parentEl){
+        const barIcon = document.createElement('div');
+        barIcon.className = 'expenses-bar__header-icon';
+        barIcon.innerHTML = categoryIcon[0];
+        parentEl.appendChild(barIcon);
+    }
+
+    createExpensesBarTitle(parentEl){
+        const title = document.createElement('h2');
+        title.className = 'expenses-bar__title'
+        title.innerHTML = "Nowa Kategoria";
+        parentEl.appendChild(title);
+    }
+
+    price(parentEl){
         const price = document.createElement('span');
-        price.className = 'bar-price';
+        price.className = 'expenses-bar__price';
 
         price.innerHTML = '500,00 zł';
         
-        this.expensesBox.appendChild(price);
+        parentEl.appendChild(price);
+    }
 
+
+    createExpensesBar(){
+        const el = []
+        
+        for (let i = 0; i <= 2; i++) {
+            const div = document.createElement('div');
+            // console.log(el);
+            el.push(div);
+        }
+        el[0].className = 'expenses-bar__inner';
+        el[1].className = 'expenses-bar__header';
+        el[2].className = 'expenses-bar__list';
+        el.forEach(elem => console.log(elem));
+
+        // EVENT CLICK 
+        el[0].addEventListener('click', function(){
+            console.log(this.children[0].firstChild);
+            this.children[0].firstChild.classList.toggle('expenses-bar__icon--hide');
+        })
+        
+
+        el[0].append(el[1]);
+        el[0].append(el[2]);
+
+        // CREATE AND ADD ELEMENTS TO EXPENSES WRAPPER 
+        this.expensesWrapper.appendChild(el[0]);
+        this.createExpensesBarIcon(el[1]);
+        this.createBarIcon(el[1]);
+        this.createExpensesBarTitle(el[1]);
+        this.price(el[1]);
     }
 }
