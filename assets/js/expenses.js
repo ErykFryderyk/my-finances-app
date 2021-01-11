e3f8c6e22e978eb8e692512d11ba0c6dffd54e63
Eryk Fryderyk <grzeniu098@gmail.com>
2021-01-11 22:00:02 +0100

    modified event which manipulate addition of classes

--- assets/js/expenses.js
+++ assets/js/expenses.js
@@ -1,16 +1,19 @@
 import { arrow, categoryIcon } from './svgfile.js';
+import { ExpensesList } from './list-expenses.js';
 
 export  class Expenses{
-    
-    expensesWrapper = document.querySelector('.expenses-bar');
-    expensesHeader = null;
+    constructor(
+        expensesBar, 
+        expensesBarList
+    ){
+        this.expensesBar = expensesBar;
+        this.expensesBarList = expensesBarList;
 
-    constructor(){
-
+        this.expensesList = null;
     }
 
-    createElement(element, type, nameClass ){
-        element = document.createElement(`'${type}'`);
+    createElement(element, type, nameClass){
+        element = document.createElement(`.${type}`);
         element.className = nameClass;
     }
 
@@ -41,9 +44,7 @@ export  class Expenses{
     price(parentEl){
         const price = document.createElement('span');
         price.className = 'expenses-bar__price';
-
         price.innerHTML = '500,00 zł';
-        
         parentEl.appendChild(price);
     }
 
@@ -53,18 +54,22 @@ export  class Expenses{
         
         for (let i = 0; i <= 2; i++) {
             const div = document.createElement('div');
-            // console.log(el);
             el.push(div);
         }
         el[0].className = 'expenses-bar__inner';
         el[1].className = 'expenses-bar__header';
         el[2].className = 'expenses-bar__list';
-        el.forEach(elem => console.log(elem));
+        // el.forEach(elem => console.log(elem));
 
         // EVENT CLICK 
-        el[0].addEventListener('click', function(){
-            console.log(this.children[0].firstChild);
-            this.children[0].firstChild.classList.toggle('expenses-bar__icon--hide');
+        el[0].addEventListener('click', function(e){
+                if(this.children[0].firstChild.classList.contains('expenses-bar__icon--hide')){
+                    this.children[0].firstChild.classList.remove('expenses-bar__icon--hide')
+                    el[2].classList.add('expenses-bar__list--hide');
+                }else{
+                    this.children[0].firstChild.classList.add('expenses-bar__icon--hide')
+                    el[2].classList.remove('expenses-bar__list--hide');
+                }
         })
         
 
@@ -72,10 +77,14 @@ export  class Expenses{
         el[0].append(el[2]);
 
         // CREATE AND ADD ELEMENTS TO EXPENSES WRAPPER 
-        this.expensesWrapper.appendChild(el[0]);
+        this.expensesBar.appendChild(el[0]);
         this.createExpensesBarIcon(el[1]);
         this.createBarIcon(el[1]);
         this.createExpensesBarTitle(el[1]);
         this.price(el[1]);
+
+        this.expensesList = new ExpensesList(el[2]);
+        this.expensesList.createListElements(el[2]);
+        this.expensesList.createListElements(el[2]);
     }
 }
