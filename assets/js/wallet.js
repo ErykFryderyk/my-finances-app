129f223d5ae031230c23e2fd4791f9162ed5008f
Eryk Fryderyk <grzeniu098@gmail.com>
2021-01-26 12:41:22 +0100

    fix calculation method

--- assets/js/wallet.js
+++ assets/js/wallet.js
@@ -3,18 +3,18 @@ export class Wallet {
     
     constructor(elem){
         this.elem = elem;
-        this.price = 5000;
+        this.price = null;
     }
 
     calculation(){
         const prices = document.querySelectorAll('.expenses-bar__price');
+        const paymentPrice = document.querySelector('.payment__value');
         const priceArr = [];
-
+        this.price = parseInt(paymentPrice.innerHTML);
         
         prices.forEach(el =>{
             priceArr.push(parseInt(el.innerHTML));
         });
-        console.log(priceArr);
         
         let sum = 0;
         for (let i = 0; i < priceArr.length; i++) {
@@ -22,19 +22,11 @@ export class Wallet {
         }
 
         this.price = this.price - sum;
-
-        console.log(this.price);
     }
 
     setValue(){ 
-        console.log(this.elem);
-        // this.walletElem.classList.add('price');
         this.calculation();
         this.elem.innerHTML = this.price + "zł";
         
     }
 }
-
-
-
-// ZRobiłem nową klasę która umożliwa zliczanie wartości poszczególnych wydatków które dodaliśmy do listy i obliczyć ile zostało nam srodków do wykorzystania
