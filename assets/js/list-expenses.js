d04904e633174015af16418fa3b4ae85256f4e9e
Eryk Fryderyk <grzeniu098@gmail.com>
2021-01-28 21:00:35 +0100

    add delate button

--- assets/js/list-expenses.js
+++ assets/js/list-expenses.js
@@ -10,15 +10,19 @@ export class ExpensesList {
         const li = document.createElement('li');
         const h3 = document.createElement('h3');
         const span = document.createElement('span');
+        const button = document.createElement('button');
 
         span.classList.add('list-price');
         span.textContent = 200;
+        button.textContent = "Usuń";
+
 
         h3.innerHTML = `${this.counter}. Element listy `;
         this.counter++;
+        
         h3.appendChild(span);  
-
         li.appendChild(h3);
+        li.appendChild(button);
         ul.appendChild(li);
 
         this.parentEl.classList.add('expenses-bar__list--hide');
