eb33613eaf58c19dce831eb356e4cb15550ba5db
Eryk Fryderyk <grzeniu098@gmail.com>
2021-01-28 19:27:02 +0100

    add event listener

--- assets/js/navigationBar.js
+++ assets/js/navigationBar.js
@@ -1,15 +1,23 @@
 export class NavigationBar {
-    constructor(wrapper, modal, button){
+    constructor(wrapper, modalPayment, modalCategory, buttonSet, buttonAdd){
         this.wrapper = wrapper;
-        this.modal = modal;
-        this.button = button;
+        this.modalPayment = modalPayment;
+        this.modalCategory = modalCategory;
+        this.button = buttonSet;
+        this.buttonCategory = buttonAdd;
+
+        this.inputCategory;
     }
 
-    createElement(){
+    setEvents(){
         this.button.addEventListener('click', () => {
-            this.modal.classList.toggle('modal--active')
+            this.modalPayment.classList.toggle('modal--active');
         });
-        
+
+        this.buttonCategory.addEventListener('click', () => {
+            this.modalCategory.classList.toggle('modal__add-category--hide');
+        });
+
         
         const div = document.createElement('div');
         const button = document.createElement('button');
