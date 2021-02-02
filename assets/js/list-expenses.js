dbf9d064b550330214d5eae66c31a3622f3dd672
Eryk Fryderyk <grzeniu098@gmail.com>
2021-02-02 21:51:34 +0100

    modify create list method

--- assets/js/list-expenses.js
+++ assets/js/list-expenses.js
@@ -4,7 +4,7 @@ export class ExpensesList {
         this.parentEl = parentEl;
     }
 
-    createListElements(parent = this.parentEl){
+    createListElements(el = this.parentEl, expenseName = 'Element listy', expensePrice = 200){
         const ul = document.createElement('ul');
         const li = document.createElement('li');
         const h3 = document.createElement('h3');
@@ -12,19 +12,19 @@ export class ExpensesList {
         const button = document.createElement('button');
 
         span.classList.add('list-price');
-        span.textContent = 200;
+        span.textContent = `${expensePrice} zł`;
         button.textContent = "Usuń";
 
 
-        h3.innerHTML = `Element listy `;
+        h3.innerHTML = `${expenseName} `;
         
         h3.appendChild(span);  
         li.appendChild(h3);
         li.appendChild(button);
         ul.appendChild(li);
 
-        parent.classList.add('expenses-bar__list--hide');
-        parent.appendChild(ul);
+        el.classList.add('expenses-bar__list--hide');
+        el.appendChild(ul);
     }
 
     fun(){
